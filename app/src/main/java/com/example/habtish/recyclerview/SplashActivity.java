bd8591b34641c47943ad2351928fb7720d900984
Alazar Kassahun <alazar.kas@gmail.com>
2018-05-04 15:48:00 +0300

    osmelting

--- app/src/main/java/com/example/habtish/recyclerview/SplashActivity.java
+++ app/src/main/java/com/example/habtish/recyclerview/SplashActivity.java
@@ -1,13 +1,79 @@
 package com.example.habtish.recyclerview;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    Handler handler;
+    Runnable runnable;
+    Timer timer;
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        progressBar = findViewById(R.id.progressBar_id);
+        progressBar.setVisibility(View.VISIBLE);
+        progressBar.setProgress(0);
+
+        progressBar.setMax(100);
+
+        handler = new Handler();
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+
+                if(++i <= 100){
+
+                    progressBar.setProgress(i);
+
+
+                }else{
+                    timer.cancel();
+                    progressBar.setVisibility(View.GONE);
+                }
+
+            }
+        };
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(runnable);
+            }
+        }, 800, 50);
+
+        Thread welcomeThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(6000);  //Delay of 10 seconds
+                } catch (Exception e) {
+
+                } finally {
+
+                    Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
+
     }
 }
